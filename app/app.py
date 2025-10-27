7332f151f49eb5262034f4ec7130b97d82b75cec
Militão <44532067+M1L1T40324@users.noreply.github.com>
2025-10-27 11:16:37 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -93,7 +93,7 @@ for ticker in tickers:
         y = df['Close'].values.reshape(-1, 1)
         model = LinearRegression()
         model.fit(X, y)
-        n_days = st.number_input("Número de dias para previsão futura",
+        n_days = st.number_input("Quantos dias você quer viajar no futuro? 🤖🔮",
                                  min_value=1,
                                  max_value=365,
                                  value=5,
@@ -190,6 +190,7 @@ for ticker in tickers:
 
     except Exception as e:
         st.error(f"Erro ao processar {ticker}: {e}")
+
 
 
 
